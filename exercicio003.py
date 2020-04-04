60b5baf3d99beaf11240907fb403b4717f066130
amaraljan <amaraljan@gmail.com>
2020-04-04 20:14:15 -0300

    Meu terceiro arquivo

---
+++ exercicio003.py
@@ -0,0 +1,8 @@
+a = input ('Digite um número')
+b = input ('Digite outro número')
+p = int(a)
+s = int(b)
+soma = p + s
+print ('O primeiro número é ', p)
+print ('O segundo número é ', s)
+print ('O resultado da soma do número ', p, 'com o número ', s, 'e', soma)
